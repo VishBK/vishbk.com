fda3c707ac5fdb4437c1c69a255448486b745917
Vish B-K <vishkarthikeyan@gmail.com>
2019-08-10 16:40:59 -0500

    Added Now Playing Track

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+var myHeading = document.querySelector('h1');
+
+var lastFMData = {
+    baseURL: "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=",
+    user: "VishBK", // Last.fm username
+    apiKey: "84a282a51ad5840993f942428d0b50db",
+    optionalParams: "&format=json&limit=1"
+  };
+
+var getSetLastFMData = function () {
+    $.ajax({
+        type : "GET",
+        url : 
+            lastFMData.baseURL + 
+            lastFMData.user + 
+            "&api_key=" + 
+            lastFMData.apiKey + 
+            lastFMData.optionalParams,
+        dataType: "json",
+        success : function(data) {
+            var recentTrack = data.recenttracks.track[0];
+            $("#trackArt").attr("src", recentTrack.image[2]["#text"]);
+            $("#trackTitle").html(recentTrack.name);
+            $("#trackArtist").html(recentTrack.artist["#text"]);
+        },
+        error : function(code, message) {
+        }
+    })
+}
+  
+// Get the new one.
+getSetLastFMData();
+// Start the countdown.
+setInterval(getSetLastFMData, 10 * 1000);
