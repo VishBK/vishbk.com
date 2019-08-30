2c0aabda3ed0dba173eb7727c794525b1caf2990
Vish B-K <vishkarthikeyan@gmail.com>
2019-08-30 00:52:18 -0500

    Add header file
    Fix small issues such as being able to click outside of divs. Added "page" folder.

---
+++ src/scripts/lastfm.js
@@ -0,0 +1,35 @@
+var lastFMData = {
+  baseURL: "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=",
+  user: "VishBK", // Last.fm username
+  apiKey: "84a282a51ad5840993f942428d0b50db",
+  optionalParams: "&format=json&limit=1"
+};
+
+var getSetLastFMData = function () {
+  $.ajax({
+      type : "GET",
+      url : 
+          lastFMData.baseURL + 
+          lastFMData.user + 
+          "&api_key=" + 
+          lastFMData.apiKey + 
+          lastFMData.optionalParams,
+      dataType: "json",
+      success : function(data) {            
+          var recentTrack = data.recenttracks.track[0];
+          if (recentTrack.hasOwnProperty("@attr")) {  // Check if @attr and therefore nowplaying exists
+              $("#listeningTitle").html("Listening To");
+          } else {
+              $("#listeningTitle").html("Last Listened To");
+          }
+          $("#trackArt").attr("src", recentTrack.image[3]["#text"]);
+          $("#trackTitle").html(recentTrack.name);
+          $("#trackArtist").html(recentTrack.artist["#text"]);
+      },
+      error : function(code, message) {
+      }
+  })
+}
+
+getSetLastFMData();
+setInterval(getSetLastFMData, 10 * 1000);
