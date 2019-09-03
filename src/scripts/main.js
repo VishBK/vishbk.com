89f90f6e49c47ace2b31a0eb27aa0e09f4e45add
Vish B-K <vishkarthikeyan@gmail.com>
2019-09-02 22:40:08 -0500

    Added arrows for dropdowns

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
+$("#collapse-one").on("show.bs.collapse", function () {
+  $("#arrow-one").css("transform", "rotate(-135deg)");
+  $("#arrow-one").css("webkitTransform", "rotate(-135deg)");
+});
 
+$("#collapse-two").on("show.bs.collapse", function () {
+  $("#arrow-two").css("transform", "rotate(-135deg)");
+  $("#arrow-two").css("webkitTransform", "rotate(-135deg)");
+});
+
+$("#collapse-one").on("hide.bs.collapse", function () {
+  $("#arrow-one").css("transform", "rotate(45deg)");
+  $("#arrow-one").css("webkitTransform", "rotate(45deg)");
+});
+
+$("#collapse-two").on("hide.bs.collapse", function () {
+  $("#arrow-two").css("transform", "rotate(45deg)");
+  $("#arrow-two").css("webkitTransform", "rotate(45deg)");
+});
