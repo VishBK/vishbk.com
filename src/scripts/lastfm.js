fc7b4e23dd345e9ef17bb69bbb763a348f53c4c4
Vish B-K <vishkarthikeyan@gmail.com>
2019-09-02 20:55:24 -0500

    Add preloading elements
    Add elements that show before others load, round album art and music bars.

--- src/scripts/lastfm.js
+++ src/scripts/lastfm.js
@@ -19,23 +19,36 @@ var getSetLastFMData = function () {
           var recentTrack = data.recenttracks.track[0];
           if (recentTrack.hasOwnProperty("@attr")) {  // Check if @attr and therefore nowplaying exists
               $("#listeningTitle").html("Listening To");
+              $("#musicBars").css("visibility", "visible");
           } else {
               $("#listeningTitle").html("Last Listened To");
-              removeElement("musicBars");
+              $("#musicBars").css("visibility", "hidden");
           }
-          $("#trackArt").attr("src", recentTrack.image[3]["#text"]);
+          var imageSrc = recentTrack.image[3]["#text"];
+          $("#trackArt").attr("src", imageSrc);
+          //var rgb = getImageColor(imageSrc);
           $("#trackTitle").html(recentTrack.name);
           $("#trackArtist").html(recentTrack.artist["#text"]);
+          //$(".bar").css("background-color", "rgba(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ",0.75)");
       },
       error : function(code, message) {
       }
   })
 }
 
-function removeElement(elementId) {
-    // Removes an element from the document
-    var element = document.getElementById(elementId);
-    element.parentNode.removeChild(element);
+function getImageColor(imageSrc) {
+    const colorThief = new ColorThief();
+    const img = new Image();
+    img.src = imageSrc;
+
+    // Make sure image is finished loading
+    if (img.complete) {
+        return colorThief.getColor(img);
+    } else {
+        img.addEventListener("load", function() {
+            return colorThief.getColor(img);
+        });
+    }
 }
 
 getSetLastFMData();
